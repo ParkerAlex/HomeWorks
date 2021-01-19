a571b499a4045fa972ccd3f3162e81a085eabf04
KIBERS <KIBERS@192.168.43.182>
2021-01-19 10:17:30 +0300

    First commit

---
+++ Homework/src/eu/senla/homework/Main.java
@@ -0,0 +1,81 @@
+package eu.senla.homework;
+
+public class Main {
+
+	static byte a;
+	static short b;
+	static int c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
+
+	static Byte aByte;
+	static Short bShort;
+	static Integer cInteger;
+	static Long dLong;
+	static Float eFloat;
+	static Double fDouble;
+	static Character gCharacter;
+	static Boolean hBoolean;
+
+	public static void main(String[] args) {
+ 
+		
+		Number number = new Number();
+
+		number.setA((byte) 100);
+		byte a = number.getA();
+		System.out.println("byte a = " + a);
+
+		byte m = a;
+		System.out.println("byte m = " + m);
+
+		number.setB((short) 300);
+		short b = number.getB();
+		System.out.println("short b = " + number.getB());
+
+		int n = b;
+		System.out.println("int n = " + n);
+
+		float mn = m + n;
+		System.out.println("float mn = " + mn);
+
+		number.setF((double) 3333.333);
+		float u = (float)number.getF();
+		System.out.println("float u = " + u);
+		
+		long z = (long)mn + (long)u;
+	    System.out.println("long z = " + z);
+		  
+	    number.setH((boolean) true);
+		boolean h = number.getH();
+		System.out.println("boolean h = " + h);
+		
+		boolean hx = h && false;
+		System.out.println("boolean hx = " + hx);
+		
+		Integer xy = 200;
+		Integer xz = 200;
+		Boolean xyz = xy == xz;
+		Boolean xzy = xy.equals(xz);
+		System.out.println("boolean xy == xz = " + xyz);
+		System.out.println("boolean xy  xz = " + xzy);
+		  
+		
+        number.setL((String) "DOG_");
+		String l = number.getL();
+		System.out.println("String l = " + l);
+
+
+		number.setG((char) 'J');
+		char g = number.getG();
+		System.out.println("char G = " + g);
+			
+	    String lg = l + g + 'i' + "mi";
+	    System.out.println("String lg = " + lg);
+		 
+	}
+
+}
---
+++ Homework/src/eu/senla/homework/Number.java
@@ -0,0 +1,171 @@
+package eu.senla.homework;
+
+public class Number {
+	
+	private byte a;
+	private short b;
+	private int c;
+	private long d;
+	private float e;
+	private double f;
+	private char g;
+	private boolean h;
+	private String l; 
+	
+
+	public Byte aByte;
+	public Short bShort;
+	public Integer cInteger;
+	public Long dLong;
+	public Float eFloat;
+	public Double fDouble;
+	public Character gCharacter;
+	public Boolean hBoolean;
+	public String lString;
+	
+	public byte getA() {
+		  return a;
+	  }
+	  
+	  public void setA(byte a) {
+		  this.a = a;
+	  }
+
+	  public short getB() {
+		  return b;
+	  }
+	  
+	  public void setB(short b) {
+		  this.b = b;
+	  }
+	  
+	  public int getC() {
+		  return c;
+	  }
+	  
+	  public void setC(int c) {
+		  this.c = c;
+	  }
+	  
+	  public long getD() {
+		  return d;
+	  }
+	  
+	  public void setD(long d) {
+		  this.d = d;
+	  }
+
+	  public float getE() {
+		  return e;
+	  }
+	  
+	  public float setE(float e) {
+		  return this.e = e;
+	  }
+	  
+	  public double getF() {
+		  return f;
+	  }
+	  
+	  public void setF(double f) {
+		  this.f = f;
+	  }
+	  
+	  public char getG() {
+		  return g;
+	  }
+	  
+	  public void setG(char g) {
+		  this.g = g;
+	  }
+	  
+	  public boolean getH() {
+		  return h;
+	  }
+	  
+	  public void setH(boolean h) {
+		  this.h = h;
+	  }
+	  
+	  public String getL() {
+		  return l;
+	  }
+	  
+	  public void setL(String l) {
+		  this.l = l;
+	  }
+/////////////////////////////////////////////////////
+	  
+	  public Byte getaByte() {
+		  return aByte;
+	  }
+	  
+	  public void setaByte(Byte aByte) {
+		  this.aByte = aByte;
+	  }
+	  
+	  public Short getbShort() {
+		  return bShort;
+	  }
+	  
+	  public void setbShort(Short bShort) {
+		  this.bShort = bShort;
+	  }
+
+	public Integer getcInteger() {
+		return cInteger;
+	}
+
+	public void setcInteger(Integer cInteger) {
+		this.cInteger = cInteger;
+	}
+
+	public  Long getdLong() {
+		return dLong;
+	}
+
+	public  void setdLong(Long dLong) {
+		this.dLong = dLong;
+	}
+
+	public Float geteFloat() {
+		return eFloat;
+	}
+
+	public void seteFloat(Float eFloat) {
+		this.eFloat = eFloat;
+	}
+
+	public Double getfDouble() {
+		return fDouble;
+	}
+
+	public void setfDouble(Double fDouble) {
+		this.fDouble = fDouble;
+	}
+
+	public Character getgCharacter() {
+		return gCharacter;
+	}
+
+	public void setgCharacter(Character gCharacter) {
+		this.gCharacter = gCharacter;
+	}
+
+	public Boolean gethBoolean() {
+		return hBoolean;
+	}
+
+	public void sethBoolean(Boolean hBoolean) {
+		this.hBoolean = hBoolean;
+	}
+
+	public String getlString() {
+		return lString;
+	}
+
+	public void setlString(String lString) {
+		this.lString = lString;
+	}
+	  
+}
